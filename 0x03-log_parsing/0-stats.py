7267b46778dc109debb9d1571222f63a4d66926b
xaldovah <dxaldovah@gmail.com>
2024-01-26 15:17:09 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,48 +2,39 @@
 """This module reads stdin line by line and computes metrics"""
 
 import sys
-from datetime import datetime
-
 
 total_size = 0
-status_codes = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-}
+status_code_counts = {}
 line_count = 0
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        parts = line.split(' ')
+        try:
+            parts = line.split()
+            file_size = int(parts[-1])
+            status_code = int(parts[-2])
+            print("File size:", file_size)
+            print(f"{status_code}")
+        except ValueError:
+            pass
 
-        if len(parts) == 10 and parts[3].isdigit() and parts[8].isdigit():
-            ip, date_str, status_code, file_size = parts[
-                    0], parts[3], parts[8], int(parts[9])
+        if len(parts) >= 7 and parts[2] == '-[':
+            print(parts)
+            try:
+                total_size += file_size
+                status_code_counts[status_code] = status_code_counts.get(
+                        status_code, 0) + 1
+            except ValueError:
+                pass
 
-            total_size += file_size
-
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-
-            line_count += 1
-
-            # print statistics every 10 lines
-            if line_count % 10 == 0:
-                print(f'Total file size: {total_size}')
-                for code in sorted(status_codes):
-                    if status_codes[code] > 0:
-                        print(f'{code}: {status_codes[code]}')
-                print()
+            if line_count % 10 == 0 or line_count == 1:
+                print("File size:", total_size)
+                for code in sorted(status_code_counts):
+                    print(f"{code}: {status_code_counts[code]}")
+                    print()
+                    line_count = 0
 
 except KeyboardInterrupt:
-    print(f'Total file size: {total_size}')
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print(f'{code}: {status_codes[code]}')
+    print("File size:", total_size)
+    for code in sorted(status_code_counts):
+        print(f"{code}: {status_code_counts[code]}")
