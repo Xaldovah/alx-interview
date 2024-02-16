15840ee6aa14def211e905cee4de9abb08a7c371
xaldovah <dxaldovah@gmail.com>
2024-02-17 01:35:53 +0300

    script that prints all characters of a Star Wars movie

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log(`Usage: node 0-starwars_characters.js ${movieId}`);
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, async (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('API requeset failed with status:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  if (!characters || characters.length === 0) {
+    console.log('No characters found for this movie.');
+    return;
+  }
+
+  try {
+    for (const characterUrl of characters) {
+      const characterBody = await getRequest(characterUrl);
+      const character = JSON.parse(characterBody);
+      console.log(character.name);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
+
+function getRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+This directory contains a request to the starwars api to get all characters featured in the film
