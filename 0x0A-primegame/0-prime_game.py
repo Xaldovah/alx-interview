a72e0f094261f846e61b86ea9451f849eb06e2c0
xaldovah <dxaldovah@gmail.com>
2024-03-15 02:24:40 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,28 +4,6 @@ Module to determine the winner of the Prime Game
 """
 
 
-def sieve(n):
-    """
-    Generates all prime numbers up to a given number using the Sieve of
-    Eratosthenes algorithm.
-
-    Args:
-        n (int): The upper limit for generating prime numbers.
-
-    Returns:
-    list: A list of prime numbers up to and including n.
-    """
-    primes = []
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-    for p in range(2, n + 1):
-        if is_prime[p]:
-            primes.append(p)
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-    return primes
-
-
 def isWinner(x, nums):
     """
     Determines the winner of the Prime Game for multiple rounds.
@@ -38,31 +16,50 @@ def isWinner(x, nums):
             str or None: The name of the player that won the most rounds.
             If the winner cannot be determined, returns None.
     """
-    maria_wins = ben_wins = 0
 
-    for n in nums:
+    def sieve(n):
+        """
+        Generates all prime numbers up to a given number using the Sieve of
+        Eratosthenes algorithm.
+
+        Args:
+            n (int): The upper limit for generating prime numbers.
+
+        Returns:
+            list: A list of prime numbers up to and including n.
+        """
+        primes = []
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        for p in range(2, n + 1):
+            if is_prime[p]:
+                primes.append(p)
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+        return primes
+
+    def canWin(n):
+        """
+        Determines the winner of the Prime Game for a specific round.
+
+        Args:
+            n (int): The upper limit of the range for the round.
+
+        Returns:
+            str: The name of the player who wins the round.
+        """
         primes = sieve(n)
-        remaining = set(range(1, n + 1))
-
-        turn = "Maria"
-        while remaining:
-            available_primes = [p for p in primes if p in remaining]
-            if not available_primes:
-                break
-            chosen_prime = max(
-                    available_primes) if turn == "Maria" else min(
-                            available_primes)
-            remaining -= set(range(chosen_prime, n + 1, chosen_prime))
-            turn = "Ben" if turn == "Maria" else "Maria"
-
-        if turn == "Maria":
-            ben_wins += 1
+        if len(primes) % 2 == 1:
+            return "Maria"
         else:
-            maria_wins += 1
+            return "Ben"
 
+    winners = [canWin(n) for n in nums]
+    maria_wins = winners.count("Maria")
+    ben_wins = winners.count("Ben")
     if maria_wins > ben_wins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
